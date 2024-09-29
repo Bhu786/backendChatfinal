ddc87c326ec58010e181ac7d1c3f7e7dcabe5684
bhupendra <bhupendra060102@gmail.com>
2024-09-29 05:39:37 +0530

    solvef

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 // const express = require('express')// method-1
 import express from "express"; // method-2
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 dotenv.config({});
 
- 
 const PORT = process.env.PORT || 5000;
 
 // middleware
-app.use(express.urlencoded({extended:true}));
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
-const corsOption={
-    origin:'http://localhost:3000',
-    credentials:true
+const corsOption = {
+  origin: "https://66f8984ea708be7a96520c54--vocal-cendol-1c243d.netlify.app",
+  credentials: true,
 };
-app.use(cors(corsOption)); 
-
+app.use(cors(corsOption));
 
 // routes
-app.use("/api/v1/user",userRoute); 
-app.use("/api/v1/message",messageRoute);
- 
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
-server.listen(PORT, ()=>{
-    connectDB();
-    console.log(`Server listen at prot ${PORT}`);
+server.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listen at prot ${PORT}`);
 });
-
